30348297efee79792b7b7e0b24a61f1acc908a79
Agencia Cebra <contacto@cebra.cl>
2015-06-24 00:44:32 -0400

    actualizacion de filtros
    actualiacion de funcionalidades obtener() y create()... pendientes
    delete() y modify()

--- js/app.js
+++ js/app.js
@@ -2,34 +2,51 @@ var app =  angular.module('chatApp', ['firebase']);
  
 	app.controller('chatController', ['$scope','Message', function($scope,Message){
 
-			$scope.user="Guest";
- 
-			$scope.messages= Message.all;
+		var userFacebookId = 10207123163383238;
+		var idVideo = 2;
 
-			$scope.inserisci = function(message){
-				Message.create(message);
-			};
+		$scope.messages = Message.obtener(idVideo);
+
+		$scope.insertNuevo = function(nuevo){
+			
+			var texto = nuevo.mensaje;
+			var user = 'Cristian Droguett Rubio';
+			var userId = 10207123163383238;
+ 			var fecha = new Date().getTime();
+ 			var idCap = 1;
+			
+			var mensaje = {
+				"mensaje": texto,
+				"user": user,
+				"userId": userId,
+				"fecha": fecha,
+				"idCap": idCap
+			}
+
+			Message.create(mensaje);
+		};
 	}]);
  
 	app.factory('Message', ['$firebase',
 		function($firebase) {
-			var ref = new Firebase('https://kimenapp.firebaseio.com');
-			var messages = $firebase(ref.child('messages')).$asArray();
+			
+			var ref = new Firebase('<< TU URL DE LA BASE DE DATOS >>');
 
 			var Message = {
-				all: messages,
-				create: function (message) {
-					return messages.$add(message);
+				obtener: function (idVideo) {
+					return $firebase(ref.child('messages/' + idVideo)).$asArray();
+				},
+				create: function (mensaje) {
+					return $firebase(ref.child('messages/' + mensaje.idCap)).$asArray().$add(mensaje);
 				},
 				get: function (messageId) {
 					return $firebase(ref.child('messages').child(messageId)).$asObject();
 				},
-				delete: function (message) {
-					return messages.$remove(message);
+				delete: function (mensaje) {
+					return messages.$remove(mensaje);
 				}
 			};
 
 			return Message;
-
 		}
 	]);
